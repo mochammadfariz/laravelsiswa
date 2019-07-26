4e4e15b2beaa2f21f31c82c352927efb26ac2f93
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-26 11:33:37 +0700

    menampilkan tabel siswa ke view

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,2 +1,19 @@
-<h1>Halo siswa</h1>
-{{dd($data_siswa)}}
+<h1>Data siswa</h1>
+<table>
+<tr>
+    <th>Nama Depan</th>
+    <th>Nama Belakang</th>
+    <th>Jenis Kelamin</th>
+    <th>Agama</th>
+    <th>Alamat</th>
+</tr>
+@foreach($data_siswa as $siswa)
+<tr>
+<td>{{$siswa->nama_depan}}</td>
+<td>{{$siswa->nama_belakang}}</td>
+<td>{{$siswa->jenis_kelamin}}</td>
+<td>{{$siswa->agama}}</td>
+<td>{{$siswa->alamat}}</td>
+</tr>
+@endforeach
+</table>
