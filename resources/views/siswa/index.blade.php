c1119c8d02e58b5abda1043dbcb150f8e78aeb57
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-26 17:09:21 +0700

    menghias table dan button tambah data siswa

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -9,25 +9,62 @@
 </head>
 <body>
 
-<h1>Data siswa</h1>
-<table>
-<tr>
-    <th>Nama Depan</th>
-    <th>Nama Belakang</th>
-    <th>Jenis Kelamin</th>
-    <th>Agama</th>
-    <th>Alamat</th>
-</tr>
-@foreach($data_siswa as $siswa)
-<tr>
-<td>{{$siswa->nama_depan}}</td>
-<td>{{$siswa->nama_belakang}}</td>
-<td>{{$siswa->jenis_kelamin}}</td>
-<td>{{$siswa->agama}}</td>
-<td>{{$siswa->alamat}}</td>
-</tr>
-@endforeach
-</table>
+<div class="container">
+    <div class="row">
+    <div class="col-6">
+            <h1>Data siswa</h1>
+    </div>
+    <div class="col-6">
+                    <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary float-right btn-sm" data-toggle="modal" data-target="#exampleModal">
+                Tambah Data Siswa
+                </button>
+    </div>
+    
+    <table class="table table-hover">
+        <tr>
+            <th>Nama Depan</th>
+            <th>Nama Belakang</th>
+            <th>Jenis Kelamin</th>
+            <th>Agama</th>
+            <th>Alamat</th>
+        </tr>
+        @foreach($data_siswa as $siswa)
+        <tr>
+        <td>{{$siswa->nama_depan}}</td>
+        <td>{{$siswa->nama_belakang}}</td>
+        <td>{{$siswa->jenis_kelamin}}</td>
+        <td>{{$siswa->agama}}</td>
+        <td>{{$siswa->alamat}}</td>
+        </tr>
+        @endforeach
+    </table>
+    </div>
+</div>
+
+
+ <!-- Modal -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
+
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
