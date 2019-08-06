9b60a17b7a7134beeeb52f0fe4aa7da845649775
mochammadfariz <mochammadfariz11@gmail.com>
2019-08-06 11:15:53 +0700

    layouting template dengan includes dan memodifikasi isi konten sesuai dengan klorofil

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -8,37 +8,38 @@
                 <div class="col-md-12">
                     <div class="panel">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Hover Row</h3>
+                            <h3 class="panel-title">Data Siswa</h3>
+                            <div class="right">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal"><i class="lnr lnr-plus-circle"></i></button>
+                            </div>
+
                         </div>
                         <div class="panel-body">
                             <table class="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
+                                        <th>Nama Depan</th>
+                                        <th>Nama Belakang</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Agama</th>
+                                        <th>Alamat</th>
+                                        <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @foreach($data_siswa as $siswa)
                                     <tr>
-                                        <td>1</td>
-                                        <td>Steve</td>
-                                        <td>Jobs</td>
-                                        <td>@steve</td>
+                                        <td>{{$siswa->nama_depan}}</td>
+                                        <td>{{$siswa->nama_belakang}}</td>
+                                        <td>{{$siswa->jenis_kelamin}}</td>
+                                        <td>{{$siswa->agama}}</td>
+                                        <td>{{$siswa->alamat}}</td>
+                                        <td>
+                                            <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit<a />
+                                                <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Anda yakin ingin menghapus data ini?')">Delete</a>
+                                        </td>
                                     </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Simon</td>
-                                        <td>Philips</td>
-                                        <td>@simon</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Jane</td>
-                                        <td>Doe</td>
-                                        <td>@jane</td>
-                                    </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -48,50 +49,6 @@
         </div>
     </div>
 </div>
-@stop
-@section('content1')
-@if(session('sukses'))
-<div class="alert alert-success" role="alert">
-    {{session('sukses')}}
-</div>
-@endif
-<div class="row">
-    <div class="col-6">
-        <h1>Data siswa</h1>
-    </div>
-    <div class="col-6">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary float-right btn-sm" data-toggle="modal" data-target="#exampleModal">
-            Tambah Data Siswa
-        </button>
-    </div>
-
-    <table class="table table-hover">
-        <tr>
-            <th>Nama Depan</th>
-            <th>Nama Belakang</th>
-            <th>Jenis Kelamin</th>
-            <th>Agama</th>
-            <th>Alamat</th>
-            <th>Aksi</th>
-        </tr>
-        @foreach($data_siswa as $siswa)
-        <tr>
-            <td>{{$siswa->nama_depan}}</td>
-            <td>{{$siswa->nama_belakang}}</td>
-            <td>{{$siswa->jenis_kelamin}}</td>
-            <td>{{$siswa->agama}}</td>
-            <td>{{$siswa->alamat}}</td>
-            <td>
-                <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit<a />
-                    <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Anda yakin ingin menghapus data ini?')">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</div>
-</div>
-
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -142,4 +99,4 @@
             </div>
         </div>
     </div>
-    @endsection
+    @stop
