dd3e794ee1a15cd4b12b4debd1797e03b8af359a
mochammadfariz <mochammadfariz11@gmail.com>
2019-08-24 17:39:36 +0700

    membuat pivot table untuk relasi many to many dan membuat model Mapel

---
+++ app/Mapel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mapel extends Model
+{
+    protected $tabel = 'mapel';
+    protected $fillable = ['kode', 'nama', 'semester'];
+}
