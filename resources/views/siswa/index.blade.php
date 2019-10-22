72606f895a5f61d9a8268d2328af9c699276ee7f
mochammadfariz <mochammadfariz11@gmail.com>
2019-10-23 06:29:18 +0700

    fe

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -89,7 +89,7 @@
                     <div class="form-group">
                         <label for="exampleInputEmail1">Agama</label>
                         <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Agama">
-                    </div>
+                    </div>j
 
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1">Alamat</label>
